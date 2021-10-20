7fbfe8e811e14196a26dd3fb2117180fcfe40d45
Eugene Sparrow <blacksparrow56@mail.ru>
2021-10-20 22:00:13 +0300

    small corrects

---
+++ Assets/Scripts/Tools/Streaks/Streak.cs
@@ -0,0 +1,12 @@
+﻿using ESparrow.Utils.Generalization.Summation.Interfaces;
+
+namespace ESparrow.Utils.Tools.Streaks
+{
+    public class Streak<TSelf, TSummable> : StreakBase<TSelf, TSummable> where TSummable : ISummable<TSelf>
+    {
+        public Streak(ISummable<TSelf> summable, TSelf zero) : base(summable, zero)
+        {
+            
+        }
+    }
+}

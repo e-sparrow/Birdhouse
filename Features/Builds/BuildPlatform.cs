8bcb46d819fabf3f3c1e38b541c51d7ff74593cd
eSparrow <51369840+e-sparrow@users.noreply.github.com>
2022-11-05 14:54:49 +0300

    some corrects

---
+++ Features/Builds/BuildPlatform.cs
@@ -0,0 +1,18 @@
+﻿using Birdhouse.Features.Builds.Interfaces;
+using UnityEditor;
+
+namespace Birdhouse.Features.Builds
+{
+    public readonly struct BuildTarget : IBuildTarget
+    {
+        public BuildTargetGroup Group
+        {
+            get;
+        }
+
+        public BuildTarget Target
+        {
+            get;
+        }
+    }
+}

13a8416eb914b575498977af1838cd1e81d78281
eSparrow <e.sparrow56@gmail.com>
2024-03-19 21:57:11 +0300

    one more commit with minor fixes

---
+++ Features/FluentLogics/FluentLogicExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Birdhouse.Features.FluentLogics
+{
+    public static class FluentLogicExtensions
+    {
+        public static SoHandler So(this bool self, Action action)
+        {
+            var result = new LogicRoot(() => self)
+                .So(action);
+            
+            return result;
+        }
+
+        public static SoHandler So(this Func<bool> self, Action action)
+        {
+            var result = new LogicRoot(self)
+                .So(action);
+            
+            return result;
+        }
+    }
+}
---
+++ Features/Processors/Processor.cs
@@ -0,0 +1,11 @@
+﻿namespace Birdhouse.Features.Processors
+{
+    public sealed class Processor<T>
+        : ProcessorBase<T>
+    {
+        protected override T ProcessInternal(T source)
+        {
+            return source;
+        }
+    }
+}

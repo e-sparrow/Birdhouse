af3c80142d774ade31b44c668da7edc1d3458770
eSparrow <51369840+e-sparrow@users.noreply.github.com>
2024-04-09 01:26:53 +0300

    complemented fluent logic extensions

--- Experimental/FluentLogics/FluentLogicExtensions.cs
+++ Experimental/FluentLogics/FluentLogicExtensions.cs
@@ -5,27 +5,63 @@ namespace Birdhouse.Experimental.FluentLogics
 {
     public static class FluentLogicExtensions
     {
-        public static SoHandler So(this bool self, Action action)
+        public static SoHandler IfTrue(this bool self, Action action)
         {
-            var result = new LogicRoot(() => self)
-                .So(action);
-            
+            var result = new LogicRoot(() => self).So(action);
             return result;
         }
 
-        public static SoHandler So(this Func<bool> self, Action action)
+        public static SoHandler IfTrue(this Func<bool> self, Action action)
         {
-            var result = new LogicRoot(self)
-                .So(action);
-            
+            var result = new LogicRoot(self).So(action);
             return result;
+        }
+        
+        public static SoHandler IfFalse(this bool self, Action action)
+        {
+            var result = new LogicRoot(() => !self).So(action);
+            return result;
+        }
+
+        public static SoHandler IfFalse(this Func<bool> self, Action action)
+        {
+            var result = new LogicRoot(self.Reverse()).So(action);
+            return result;
+        }
+
+        public static SoHandler ThrowIfTrue(this Func<bool> self, Func<Exception> onFail = null)
+        {
+            onFail ??= () => new ArgumentException();
+
+            var result = self.IfTrue(FailHandler);
+            return result;
+            
+            void FailHandler()
+            {
+                var exception = onFail.Invoke();
+                throw exception;
+            }
+        }
+
+        public static SoHandler ThrowIfTrue(this bool self, Func<Exception> onFail = null)
+        {
+            onFail ??= () => new ArgumentException();
+
+            var result = self.IfTrue(FailHandler);
+            return result;
+            
+            void FailHandler()
+            {
+                var exception = onFail.Invoke();
+                throw exception;
+            }
         }
 
         public static SoHandler ThrowIfFalse(this Func<bool> self, Func<Exception> onFail = null)
         {
             onFail ??= () => new ArgumentException();
 
-            var result = self.Reverse().So(FailHandler);
+            var result = self.IfFalse(FailHandler);
             return result;
             
             void FailHandler()
@@ -39,7 +75,7 @@ namespace Birdhouse.Experimental.FluentLogics
         {
             onFail ??= () => new ArgumentException();
 
-            var result = (!self).So(FailHandler);
+            var result = self.IfFalse(FailHandler);
             return result;
             
             void FailHandler()
@@ -47,6 +83,30 @@ namespace Birdhouse.Experimental.FluentLogics
                 var exception = onFail.Invoke();
                 throw exception;
             }
+        }
+
+        public static SoHandler ThrowIfTrue(this Func<bool> self, Exception exception)
+        {
+            var result = self.ThrowIfTrue(() => exception);
+            return result;
+        }
+
+        public static SoHandler ThrowIfTrue(this bool self, Exception exception)
+        {
+            var result = self.ThrowIfTrue(() => exception);
+            return result;
+        }
+
+        public static SoHandler ThrowIfFalse(this Func<bool> self, Exception exception)
+        {
+            var result = self.ThrowIfFalse(() => exception);
+            return result;
+        }
+
+        public static SoHandler ThrowIfFalse(this bool self, Exception exception)
+        {
+            var result = self.ThrowIfFalse(() => exception);
+            return result;
         }
     }
 }

c81c8511ed75a19c0a5202a0d81a415427fbc8b6
e-sparrow <51369840+e-sparrow@users.noreply.github.com>
2024-05-03 15:29:14 +0300

    added a signal bus

---
+++ Tools/Signals/SignalBus.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Birdhouse.Abstractions.Disposables;
+
+namespace Birdhouse.Tools.Signals
+{
+    public class SignalBus<TBase>
+    {
+        private readonly IDictionary<Type, ICollection<Action<object>>> _handlers 
+            = new Dictionary<Type, ICollection<Action<object>>>(); 
+        
+        public void Fire<T>(T argument)
+            where T : TBase
+        {
+            var hasHandlers = _handlers.TryGetValue(typeof(T), out var handlers);
+            if (hasHandlers)
+            {
+                foreach (var handler in handlers)
+                {
+                    handler.Invoke(argument);
+                }
+            }
+        }
+
+        public IDisposable Subscribe<T>(Action<T> handler)
+            where T : TBase
+        {
+            var result = new Action<object>(value => handler.Invoke((T) value));
+            
+            var hasHandlers = _handlers.TryGetValue(typeof(T), out var handlers);
+            if (hasHandlers)
+            {
+                handlers.Add(result);
+            }
+            else
+            {
+                handlers = new List<Action<object>>();
+                handlers.Add(result);
+                
+                _handlers[typeof(T)] = handlers;
+            }
+            
+            var token = new CallbackDisposable(Remove);
+            return token;
+
+            void Remove()
+            {
+                handlers.Remove(result);
+                if (!handlers.Any())
+                {
+                    _handlers.Remove(typeof(T));
+                }
+            }
+        }
+    }
+    
+    public sealed class SignalBus 
+        : SignalBus<object>
+    {
+    
+    }
+}

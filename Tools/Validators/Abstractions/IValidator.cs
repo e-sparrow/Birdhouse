df88c3a999b240abcd9619805b975f42877e0e4d
e-sparrow <51369840+e-sparrow@users.noreply.github.com>
2024-05-08 20:06:03 +0300

    started working on validators

---
+++ Tools/Validators/Abstractions/IValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace Birdhouse.Tools.Validators.Abstractions
+{
+    public interface IValidator<in TIn, out TOut>
+    {
+        TOut Validate(TIn input);
+    }
+
+    public interface IValidator<in TIn>
+        : IValidator<TIn, bool>
+    {
+        
+    }
+
+    public interface IValidator<in TIn1, in TIn2, out TOut>
+    {
+        TOut Validate(TIn1 input1, TIn2 input2);
+    }
+}
---
+++ Tools/Validators/ValidatorBase.cs
@@ -0,0 +1,16 @@
+﻿using Birdhouse.Tools.Validators.Abstractions;
+
+namespace Birdhouse.Tools.Validators
+{
+    public abstract class ValidatorBase<TIn, TOut>
+        : IValidator<TIn, TOut>
+    {
+        public abstract TOut Validate(TIn input);
+    }
+    
+    public abstract class ValidatorBase<TIn1, TIn2, TOut>
+        : IValidator<TIn1, TIn2, TOut>
+    {
+        public abstract TOut Validate(TIn1 input1, TIn2 input2);
+    }
+}

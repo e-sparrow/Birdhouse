c9c25aa077c27aa296fe87d20318040fddefb2ed
e-sparrow <51369840+e-sparrow@users.noreply.github.com>
2024-12-04 16:32:53 +0300

    added protected dictionary to scriptable storage

--- Tools/Storages/Scriptable/ScriptableStorageGenericBase.cs
+++ Tools/Storages/Scriptable/ScriptableStorageGenericBase.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Birdhouse.Common.Generic.Pairs;
-using Birdhouse.Common.Generic.Pairs.Interfaces;
 using UnityEngine;
 
 namespace Birdhouse.Tools.Storages.Scriptable
@@ -10,13 +10,19 @@ namespace Birdhouse.Tools.Storages.Scriptable
         : ScriptableStorageBase<TKey, TValue>
     {
         [SerializeField] private List<SerializablePair<TKey, TValue>> data;
-        
+
+        protected Lazy<IReadOnlyDictionary<TKey, TValue>> Dictionary 
+            => new Lazy<IReadOnlyDictionary<TKey, TValue>>(CreateDictionary);
+
         public override TValue GetValue(TKey key)
         {
-            var result = data
-                .First(value => value.Key.Equals(key))
-                .Value;
-
+            var result = Dictionary.Value[key];
+            return result;
+        }
+        
+        private IReadOnlyDictionary<TKey, TValue> CreateDictionary()
+        {
+            var result = data.ToDictionary(value => value.Key, value => value.Value);
             return result;
         }
     }

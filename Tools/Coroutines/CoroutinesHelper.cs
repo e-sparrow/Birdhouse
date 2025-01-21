06b87f8659ca2299707dc15cfcf285d56fadd931
e-sparrow <51369840+e-sparrow@users.noreply.github.com>
2025-01-21 11:27:47 +0300

    runtime coroutine starters

--- Tools/Coroutines/CoroutinesHelper.cs
+++ Tools/Coroutines/CoroutinesHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Birdhouse.Tools.Coroutines.Interfaces;
 using Birdhouse.Tools.Ticks;
+using UnityEngine;
 
 namespace Birdhouse.Tools.Coroutines
 {
@@ -10,16 +11,40 @@ namespace Birdhouse.Tools.Coroutines
         private static readonly Lazy<TickCoroutineStarter> LazyCoroutineStarter 
             = new Lazy<TickCoroutineStarter>(CreateCoroutineStarter);
 
+        private static readonly Lazy<TickCoroutineStarter> LazyRuntimeCoroutineStarter
+            = new Lazy<TickCoroutineStarter>(CreateRuntimeCoroutineStarter);
+
         public static ICoroutineStarter<IEnumerator<ICoroutineInstruction>, IDisposable> TokenCoroutineStarter 
             => LazyCoroutineStarter.Value;
 
         public static ICoroutineStarter<IEnumerator<ICoroutineInstruction>> CoroutineStarter 
             => LazyCoroutineStarter.Value;
 
+        public static ICoroutineStarter<IEnumerator<ICoroutineInstruction>, IDisposable> TokenRuntimeCoroutineStarter 
+            => LazyRuntimeCoroutineStarter.Value;
+
+        public static ICoroutineStarter<IEnumerator<ICoroutineInstruction>> RuntimeCoroutineStarter =>
+            LazyRuntimeCoroutineStarter.Value;
+        
         private static TickCoroutineStarter CreateCoroutineStarter()
         {
             var result = new TickCoroutineStarter(TickHelper.GetDefaultTickProvider());
             return result;
+        }
+        
+        private static TickCoroutineStarter CreateRuntimeCoroutineStarter()
+        {
+            var result = new TickCoroutineStarter(TickHelper.GetDefaultTickProvider());
+            
+            Application.quitting += HandleQuit;
+            
+            return result;
+
+            void HandleQuit()
+            {
+                Application.quitting -= HandleQuit;
+                result.Dispose();
+            }
         }
     }
 }
